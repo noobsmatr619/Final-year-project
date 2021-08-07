08e597fe64bade7b1633fda054288a302d94d8c8
Abrar Tajwar <s.tajwar@students.plymouth.ac.uk>
2021-08-07 17:56:28 +0100

    fix travis

--- client/src/history.js
+++
@@ -1,6 +0,0 @@
-//old
-// import createHistory from "history/createBrowserHistory";
-// export default createHistory();
-//new one
-import { createBrowserHistory } from "history";
-export default createBrowserHistory(); 
