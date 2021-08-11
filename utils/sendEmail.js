5c2915024b4734d0430cb9e9a4187a78bb9d09aa
Abrar Tajwar <s.tajwar@students.plymouth.ac.uk>
2021-08-12 00:18:28 +0100

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -8,6 +8,8 @@ const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
+    secure: false,
+    requireTLS: true,
     auth: {
       user: process.env.SMTP_EMAIL,
       pass: process.env.SMTP_PASSWORD
