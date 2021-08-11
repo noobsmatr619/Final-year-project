1328f207af300284626ce9fe0eb72bd1aea8e3f4
Abrar Tajwar <s.tajwar@students.plymouth.ac.uk>
2021-08-11 23:29:48 +0100

    dump

--- routes/salaries.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { paySalary, getSalaries } = require("./../controllers/salaries");
-const { protect } = require("./../middleware/auth");
-router.get("/getSalaries", protect, getSalaries);
-router.post("/paySalary", protect, paySalary);
-
-module.exports = router;
